989a732e9bcac16078fa31457e0018bb6c88bcbd
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-23 23:19:20 +0300

    fix: correct gamemode deactivation message and ensure waybar is managed properly

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -3,8 +3,10 @@
 
 if [ -f /tmp/gamemode-enabled ]; then
   hyprctl reload
+  # enable waybar
+  waybar &
   rm /tmp/gamemode-enabled
-  notify-send "Gamemode deactivated" "Animations and blur enabled"
+  notify-send "Gamemode deactivated" "Default settings restored"
 else
   hyprctl --batch "\
         keyword animations:enabled 0;\
@@ -16,6 +18,8 @@ else
         keyword decoration:inactive_opacity 1;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
+  # disable waybar
+  killall waybar
   touch /tmp/gamemode-enabled
   notify-send "Gamemode activated" "Animations and blur disabled"
 fi
