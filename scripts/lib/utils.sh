bf1361f2c27cb2dac97535b98b67cd6958c00819
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-24 20:13:11 +0300

    feat: add install_package and set_snapper_config_value functions for package management and snapper configuration

--- scripts/lib/utils.sh
+++ scripts/lib/utils.sh
@@ -27,6 +27,18 @@ fail() {
   printc red "$1"
   exit "${2:-1}"
 }
+install_package() {
+  if yay -Qi "$1" &>/dev/null; then
+    printc green "$1 is already installed."
+  else
+    if yay -S --needed --noconfirm "$1"; then
+      printc green "$1 installed successfully."
+    else
+      fail "Failed to install $1."
+    fi
+  fi
+
+}
 
 update_or_append_config() {
   local config_file="$1"
@@ -56,5 +68,23 @@ update_or_append_config() {
     else
       fail "Failed to append $key"
     fi
+  fi
+}
+
+set_snapper_config_value() {
+  local config_name="$1"
+  local key="$2"
+  local value="$3"
+
+  # Check if config exists
+  if ! sudo snapper list-configs | awk '{print $1}' | grep -qx "$config_name"; then
+    fail "Snapper config '$config_name' does not exist."
+  fi
+
+  # Set config value
+  if sudo snapper -c "$config_name" set-config "${key}=${value}"; then
+    printc green "Set $key=$value in snapper config '$config_name'"
+  else
+    fail "Failed to set $key in snapper config '$config_name'"
   fi
 }
