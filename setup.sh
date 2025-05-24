b8f91b8af53563e0dfa8c8f4ef846d4b43e9d55e
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-24 20:12:17 +0300

    feat: add confirmation prompts for ZSH and limine bootloader setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# shellcheck disable=SC1091
+
 BASE_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 SCRIPTS_DIR="$BASE_DIR/scripts"
 LIB_DIR="$SCRIPTS_DIR/lib"
+
 source "$LIB_DIR/utils.sh"
 
 # Source dependencies arrays
@@ -13,5 +16,18 @@ source "$SCRIPTS_DIR/04_fonts.sh"
 source "$SCRIPTS_DIR/05_cursor_theme.sh"
 source "$SCRIPTS_DIR/06_misc.sh"
 source "$SCRIPTS_DIR/07_dotfiles.sh"
-source "$SCRIPTS_DIR/08_setup_zsh.sh"
-source "$SCRIPTS_DIR/09_limine_snapper.sh"
+
+if confirm "Setup ZSH and related tools?"; then
+  printc yellow "Starting ZSH setup..."
+
+  source "$SCRIPTS_DIR/08_setup_zsh.sh"
+else
+  printc yellow "Skipping ZSH setup."
+fi
+
+if confirm "This is for limine bootloader setup. Do you want to continue?"; then
+  printc yellow "Starting limine bootloader setup..."
+  source "$SCRIPTS_DIR/09_limine_snapper.sh"
+else
+  printc yellow "Skipping limine bootloader setup."
+fi
