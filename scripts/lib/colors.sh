b5b528c58f8d513a654c099ced3e173e5e1bc4e5
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-25 23:52:46 +0300

    fix: correct color definitions and formatting in colors.sh

--- scripts/lib/colors.sh
+++ scripts/lib/colors.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# --- Color Map ---
+# # --- Color Map ---
 # shellcheck disable=SC2034
 declare -A COLORS=(
+  [reset]='\033[0m'
+  [black]='\033[0;30m'
   [red]='\033[0;31m'
   [green]='\033[0;32m'
-  [yellow]='\033[1;33m'
-  [white]='\033[0m'
-  [cyan]='\033[0;36m'
+  [yellow]='\033[0;33m'
   [blue]='\033[0;34m'
   [magenta]='\033[0;35m'
+  [cyan]='\033[0;36m'
+  [white]='\033[1;37m'
 )
