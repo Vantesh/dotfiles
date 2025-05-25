599dec7242ca0e6d39ea28428a094a6d81cd8021
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-25 22:11:54 +0300

    feat: expand color map to include cyan, blue, and magenta

--- scripts/lib/colors.sh
+++ scripts/lib/colors.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # --- Color Map ---
+# shellcheck disable=SC2034
 declare -A COLORS=(
   [red]='\033[0;31m'
   [green]='\033[0;32m'
   [yellow]='\033[1;33m'
   [white]='\033[0m'
+  [cyan]='\033[0;36m'
+  [blue]='\033[0;34m'
+  [magenta]='\033[0;35m'
 )
