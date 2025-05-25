edb824206fc7f350fa258d2d81ea3fdc7b74a94b
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-25 22:14:39 +0300

    feat: add additional dependencies and setup ZSH environment file

--- scripts/08_setup_zsh.sh
+++ scripts/08_setup_zsh.sh
@@ -9,6 +9,9 @@ deps=(
   eza
   fd
   less
+  ripgrep
+  bat
+  fzf
 
 )
 
@@ -16,3 +19,24 @@ deps=(
 for dep in "${deps[@]}"; do
   install_package "$dep"
 done
+
+# setup zshenv
+ZSHENV_FILE="/etc/zsh/zshenv"
+if [[ ! -d "/etc/zsh" ]]; then
+  sudo mkdir -p /etc/zsh
+fi
+printc cyan "Setting up $ZSHENV_FILE..."
+sudo tee "$ZSHENV_FILE" >/dev/null <<EOF
+# ZSH environment file
+if [[ -d "$XDG_CONFIG_HOME/zsh" ]]; then
+  export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+else
+  export ZDOTDIR="$HOME/.config/zsh"
+fi
+EOF
+
+if [[ -f "$ZSHENV_FILE" ]]; then
+  printc green "ZSH environment file created at $ZSHENV_FILE"
+else
+  fail "Failed to create ZSH environment file at $ZSHENV_FILE"
+fi
