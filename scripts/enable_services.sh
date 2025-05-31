9d575ba7e430d6dc0ad902034a0d7a19579b1b36
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-05-31 03:09:24 +0300

    feat: remove auto_cpufreq function and its service from enable_services script

--- scripts/enable_services.sh
+++ scripts/enable_services.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
-auto_cpufreq() {
-  printc yellow "Running miscellaneous setup tasks..."
-
-  if ! has_cmd auto-cpufreq; then
-    printc yellow "Installing auto-cpufreq..."
-
-    local tmp_dir
-    tmp_dir=$(mktemp -d) || fail "Failed to create temp directory for auto-cpufreq."
-
-    git clone https://github.com/AdnanHodzic/auto-cpufreq.git "$tmp_dir" || {
-      rm -rf "$tmp_dir"
-      fail "Failed to clone auto-cpufreq repo."
-    }
-
-    pushd "$tmp_dir" >/dev/null || {
-      rm -rf "$tmp_dir"
-      fail "Failed to enter auto-cpufreq directory."
-    }
-
-    sudo ./auto-cpufreq-installer || {
-      popd >/dev/null || exit 1
-      rm -rf "$tmp_dir"
-      fail "auto-cpufreq installation failed."
-    }
-
-    popd >/dev/null || exit 1
-
-    rm -rf "$tmp_dir"
-
-    sudo auto-cpufreq --install || fail "Failed to finalize auto-cpufreq installation."
-
-    printc green "auto-cpufreq installed successfully."
-  else
-    printc green "auto-cpufreq is already installed."
-  fi
-}
 
 enable_services() {
   local user_services=(
@@ -48,7 +12,6 @@ enable_services() {
     bluetooth.service
     paccache.timer
     sddm.service
-    auto-cpufreq.service
   )
 
   for service in "${user_services[@]}"; do
@@ -69,5 +32,5 @@ enable_services() {
     fi
   done
 }
-auto_cpufreq
+
 enable_services
