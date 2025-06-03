e7c1c0fb826cca275874d0daf51f0bdfb315d659
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-06-04 01:25:42 +0300

    fix: update wallpaper transition settings in randomwall.sh

--- .config/hypr/scripts/randomwall.sh
+++ .config/hypr/scripts/randomwall.sh
@@ -8,10 +8,10 @@ WALLPAPER="$WALLPAPER_DIR/$random"
 
 if pgrep -x "hyprpaper" >/dev/null; then
   hyprctl hyprpaper reload ,"$WALLPAPER" && notify-send "Wallpaper Changed" -i "$WALLPAPER" --app-name=Wallpaper
-elif pgrep -x "swww" >/dev/null; then
+else
   swww img "$WALLPAPER" \
     --transition-bezier 0.5,1.19,.8,.4 \
-    --transition-type wipe \
-    --transition-duration 2 \
-    --transition-fps 75 && notify-send "Wallpaper Changed" -i "$WALLPAPER" --app-name=Wallpaper
+    --transition-type grow \
+    --transition-duration 1 \
+    --transition-fps 60 && notify-send "Wallpaper Changed" -i "$WALLPAPER" --app-name=Wallpaper
 fi
