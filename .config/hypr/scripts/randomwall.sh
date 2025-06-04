314b6746afc9be20b6bd9242db759a9cbde2dff7
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-06-04 23:26:42 +0300

    refactor: update wallpaper transition effects to fade

--- .config/hypr/scripts/randomwall.sh
+++ .config/hypr/scripts/randomwall.sh
@@ -10,8 +10,7 @@ if pgrep -x "hyprpaper" >/dev/null; then
   hyprctl hyprpaper reload ,"$WALLPAPER" && notify-send "Wallpaper Changed" -i "$WALLPAPER" --app-name=Wallpaper
 else
   swww img "$WALLPAPER" \
-    --transition-bezier 0.5,1.19,.8,.4 \
-    --transition-type grow \
-    --transition-duration 1 \
+    --transition-type fade \
+    --transition-duration 2 \
     --transition-fps 60 && notify-send "Wallpaper Changed" -i "$WALLPAPER" --app-name=Wallpaper
 fi
