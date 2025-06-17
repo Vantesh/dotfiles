660ed997c11cf04de9560387ced94578b1e87230
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-06-18 02:22:29 +0300

    feat: add Stray theme for SDDM

---
+++ sddm/stray/Components/sessions.js
@@ -0,0 +1,17 @@
+/* Include:
+    icon field to display the icon
+    name field to display the value instead of the session name
+*/
+var sessionIcons = {
+    "Plasma (Wayland)": {
+        icon: "../icons/kde.svg",
+        name: "Plasma",
+    }, 
+    "Hyprland (uwsm-managed)": {
+	    icon: "../icons/hyprland.svg"
+    },
+    "Hyprland": {
+        icon: "../icons/hyprland.svg"
+    },
+    "Niri": {}
+}
