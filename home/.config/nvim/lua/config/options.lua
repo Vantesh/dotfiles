ed62684d7aa06cf1ecb16883ab9ad07bdc6f6ab6
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-07-04 22:03:04 +0300

    feat: update Neovim configuration and plugins

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -4,6 +4,34 @@
 vim.opt.number = true          -- keep absolute line numbers
 vim.opt.relativenumber = false -- disable relative ones
 
+local go = vim.g
+local o = vim.opt
+
+-- Autoformat on save (Global)
+go.autoformat = true
+
+
+-- Font
+go.gui_font_default_size = 10
+go.gui_font_size = go.gui_font_default_size
+go.gui_font_face = "MonoLisa Nerd Font"
+
+-- Optimizations on startup
+vim.loader.enable()
+
+-- Disable annoying cmd line stuff
+o.showcmd = false
+o.laststatus = 3
+o.cmdheight = 0
+
+-- Smoothscroll
+if vim.fn.has("nvim-0.10") == 1 then
+  o.smoothscroll = true
+end
+
+o.conceallevel = 2
+
+
 -- set curysor to beam and blink only in insert mode
 vim.opt.guicursor = {
   "n-c-sm:block",
--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,6 @@
 -- Catppuccin mocha
+local palette = require("catppuccin.palettes").get_palette("mocha") -- Import your favorite catppuccin colors
+
 return {
   {
     "LazyVim/LazyVim",
@@ -63,12 +65,56 @@ return {
     specs = {
       {
         "akinsho/bufferline.nvim",
+        opts = {
+          highlights = require("catppuccin.groups.integrations.bufferline").get({
+            styles = { "italic", "bold" },
+            custom = {
+              all = {
+                fill = {
+                  bg = palette.mantle,
+                },
+                separator_selected = {
+                  bg = palette.base,
+                  fg = palette.mantle,
+                },
+                separator = {
+                  bg = palette.mantle,
+                  fg = palette.mantle,
+                },
+                tab_separator = {
+                  bg = palette.mantle,
+                  fg = palette.mantle,
+                },
+                tab_selected = {
+                  bg = palette.base,
+                },
+                tab_separator_selected = {
+                  bg = palette.base,
+                  fg = palette.mantle,
+                },
+              },
+            },
+          }),
+        },
+      },
+      {
+        "rasulomaroff/reactive.nvim",
         optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-          end
-        end,
+        opts = {
+          load = { "catppuccin-mocha-cursor", "catppuccin-mocha-cursorline" },
+        },
+      },
+      {
+        "rachartier/tiny-devicons-auto-colors.nvim",
+        optional = true,
+        opts = {
+          colors = palette,
+          factors = {
+            lightness = 0.9,
+            chroma = 1,
+            hue = 0.7,
+          },
+        },
       },
 
     },
