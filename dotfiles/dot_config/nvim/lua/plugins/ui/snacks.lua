a4d4b335cc416bc6a31e2bf10f870df00d47f124
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-07-19 19:21:10 +0300

    feat: enhance Treesitter configuration and add new plugins

--- dotfiles/dot_config/nvim/lua/plugins/ui/snacks.lua
+++ dotfiles/dot_config/nvim/lua/plugins/ui/snacks.lua
@@ -5,6 +5,7 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
+    explorer = { enabled = true },
     dashboard = {
       enabled = true,
       preset = {
@@ -23,9 +24,9 @@ return {
     },
     indent = {
       priority = 1,
-      enabled = true,    -- enable indent guides
+      enabled = true,       -- enable indent guides
       char = "│",
-      only_scope = false, -- only show indent guides of the scope
+      only_scope = false,   -- only show indent guides of the scope
       only_current = false, -- only show indent guides in the current window
       hl = "SnacksIndent", ---@type string|string[] hl groups for indent guides
       -- can be a list of hl groups to cycle through
@@ -54,7 +55,7 @@ return {
       style = "out",
       easing = "inOutCubic",
       duration = {
-        step = 10, -- ms per step
+        step = 10,   -- ms per step
         total = 500, -- maximum duration
       },
     },
@@ -63,7 +64,7 @@ return {
       enabled = true, -- enable highlighting the current scope
       priority = 200,
       char = "│",
-      underline = false, -- underline the start of the scope
+      underline = false,    -- underline the start of the scope
       only_current = false, -- only show scope in the current window
       hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
     },
