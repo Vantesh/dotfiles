7fbce91546a3465b21314aed0552801d55f1a4c5
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-07-20 23:48:13 +0300

    feat: rewrite dependencies handling scripts; remove unnecessary configs

---
+++ dotfiles/.chezmoiscripts/run_once_before_system_init.sh
@@ -0,0 +1,190 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC1091
+source "${CHEZMOI_WORKING_TREE:?env variable missing. Please only run this script via chezmoi}/dotfiles/.chezmoiscripts/.00_helpers"
+
+source "${CHEZMOI_WORKING_TREE}/packages"
+
+dependencies=(
+  "${core[@]}"
+  "${fonts[@]}"
+  "${theming[@]}"
+)
+optional=(
+  "${optional[@]}"
+)
+
+drivers=(
+  "${drivers[@]}"
+)
+
+# =============================================================================
+# Initialize Environment
+# =============================================================================
+
+if [[ $EUID -eq 0 ]]; then
+  print_error "This script should not be run as root. Please run it as a normal user."
+fi
+
+ask_for_sudo
+
+if initialize_env; then
+  print_info "Starting setup script..."
+  clear
+else
+  print_error "Failed to initialize environment."
+fi
+
+# =============================================================================
+# Welcome Message
+# =============================================================================
+print_box "smslant" "WELCOME"
+
+echo -e "\n---------------------------------------------"
+echo -e "Hyprland Dotfiles Setup"
+echo -e "Copyright 2025 \e]8;;https://github.com/vantesh/dotfiles\a${STYLE_BOLD}${COLOR_CYAN}[Vantesh]${COLOR_RESET}\e]8;;\a"
+echo -e "This script will heavily modify your system."
+echo -e "\nPackages: ${COLOR_MAGENTA}${#dependencies[@]}${COLOR_RESET}"
+echo -e "---------------------------------------------\n"
+
+if ! confirm "Do you want to continue?"; then
+  print_info "Setup aborted by user."
+  exit 0
+else
+  clear
+
+fi
+# =============================================================================
+# Install dependencies
+# =============================================================================
+print_box "smslant" "Dependencies"
+print_step "Installing dependencies"
+
+failed_packages=()
+
+for pkg in "${dependencies[@]}"; do
+  install_package "$pkg"
+done
+
+# install drivers
+if confirm "Do you want to install drivers?"; then
+  print_step "Installing drivers"
+  for driver in "${drivers[@]}"; do
+    install_package "$driver"
+  done
+fi
+
+if confirm "Do you want to install optional packages?"; then
+  print_step "Installing optional packages"
+  for pkg in "${optional[@]}"; do
+    install_package "$pkg"
+  done
+fi
+
+if [[ ${#failed_packages[@]} -gt 0 ]]; then
+  if confirm "Some packages failed to install. Do you want to retry manually?"; then
+    print_step "Retrying failed packages"
+    for failed_pkg in "${failed_packages[@]}"; do
+      "$AUR_HELPER" -S "$failed_pkg"
+    done
+  else
+    print_warning "Skipping failed packages."
+  fi
+
+fi
+
+# =============================================================================
+# SETUP PACMAN
+# =============================================================================
+print_box "smslant" "Pacman"
+print_step "Setting up Pacman configuration"
+
+pacman_conf="/etc/pacman.conf"
+lines_to_edit=(
+  "Color"
+  "VerbosePkgLists"
+  "ParrallelDownloads"
+)
+if create_backup "$pacman_conf"; then
+  print_info "Pacman backup created"
+
+  for line in "${lines_to_edit[@]}"; do
+    if grep -q "^#\s*$line" "$pacman_conf"; then
+      sudo sed -i "s/^#\s*$line/$line/" "$pacman_conf"
+      print_info "Uncommented ${COLOR_CYAN}$line${COLOR_RESET}"
+    else
+      print_info "$COLOR_CYAN$line${COLOR_RESET} is already uncommented"
+    fi
+  done
+
+  if grep -q "^ParallelDownloads" "$pacman_conf" && ! grep -q "^ILoveCandy" "$pacman_conf"; then
+    sudo sed -i "/^ParallelDownloads/a ILoveCandy" "$pacman_conf"
+    print_info "Added ILoveCandy to $pacman_conf"
+  else
+    print_info "${COLOR_CYAN}ILoveCandy${COLOR_RESET} is already present"
+  fi
+fi
+
+paccache_config="/etc/conf.d/pacman-contrib"
+if update_config "$paccache_config" "PACCACHE_ARGS" "'-k1'"; then
+  print_info "Updated paccache configuration"
+  enable_service "paccache.timer" "system"
+else
+  print_warning "Failed to update paccache configuration"
+fi
+
+# mirrorlist
+enable_service "reflector.timer" "system"
+
+write_system_config "/etc/pacman.d/hooks/00-paccache.hook" "Paccache hook" <<EOF
+[Trigger]
+Type = Package
+Operation = Remove
+Operation = Install
+Operation = Upgrade
+Target = *
+
+[Action]
+Description = Cleaning pacman cache...
+When = PostTransaction
+Exec = /usr/bin/paccache -rk1
+Depends = pacman-contrib
+EOF
+write_system_config "/etc/pacman.d/hooks/01-paccache-uninstalled.hook" "Paccache uninstalled hook" <<EOF
+[Trigger]
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+Description = Cleaning pacman cache for uninstalled packages...
+When = PostTransaction
+Exec = /usr/bin/paccache -ruk0
+Depends = pacman-contrib
+EOF
+
+# =============================================================================
+# SUDOERS
+# =============================================================================
+print_box "smslant" "Sudo"
+print_step "Setting up sudoers configuration"
+
+write_system_config "/etc/sudoers.d/timeout" "Sudo timeout configuration" <<EOF
+Defaults passwd_timeout=0
+EOF
+
+declare -A faillock_config=(
+  [deny]="20"
+  [unlock_time]="300"
+  [fail_interval]="900"
+)
+
+for key in "${!faillock_config[@]}"; do
+  if update_config "/etc/security/faillock.conf" "$key" "${faillock_config[$key]}"; then
+    print_info "Updated faillock configuration: $key=${faillock_config[$key]}"
+  else
+    print_error "Failed to update faillock configuration for $key"
+  fi
+done
+
+# =============================================================================
