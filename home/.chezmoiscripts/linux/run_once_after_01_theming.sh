d146d3e591b90cdd76faec21b8ad6dcc9d8a8daa
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-08-25 22:15:40 +0300

    feat: enhance theming setup for GTK and QT
    - Updated  theme setup to include QT configuration.
    - Removed obsolete qt5ct and qt6ct template files.

--- home/.chezmoiscripts/linux/run_once_after_01_theming.sh
+++ home/.chezmoiscripts/linux/run_once_after_01_theming.sh
@@ -153,16 +153,57 @@ fi
 #===================================================================================
 # GTK Theme
 #===================================================================================
-print_box "smslant" "GTK Theme"
+print_box "smslant" "GTK & QT"
 print_step "Setting up GTK theme"
-print_info "Setting GTK theme"
-
 
 if gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark" &&
   gsettings set org.gnome.desktop.interface font-name "SF Pro Text 12" &&
-  gsettings set org.gnome.nautilus.preferences show-hidden-files true &&
   gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"; then
   print_info "GTK theme and icons set successfully"
 else
   print_warning "Failed to set GTK theme or icons"
 fi
+
+# ==================================================================================
+# QT THEME
+# ==================================================================================
+print_step "Setting up QT theme"
+
+for version in qt5ct qt6ct; do
+  config_dir="$HOME/.config/$version"
+  config_file="$config_dir/${version}.conf"
+
+  if [[ -f "$config_file" ]]; then
+    print_warning "$version theme already configured, skipping"
+  else
+    mkdir -p "$config_dir"
+    cat <<EOF >"$config_file"
+[Appearance]
+color_scheme_path=${HOME}/.local/share/color-schemes/Matugen.colors
+custom_palette=true
+icon_theme=
+standard_dialogs=default
+style=kvantum-dark
+
+[Fonts]
+fixed="JetBrainsMono Nerd Font,12,-1,5,50,0,0,0,0,0,Regular"
+general="SF Pro Text,12,-1,5,50,0,0,0,0,0,Regular"
+
+[Interface]
+activate_item_on_single_click=1
+buttonbox_layout=0
+cursor_flash_time=1000
+dialog_buttons_have_icons=1
+double_click_interval=400
+gui_effects=@Invalid()
+keyboard_scheme=2
+menus_have_icons=true
+show_shortcuts_in_context_menus=true
+stylesheets=@Invalid()
+toolbutton_style=4
+underline_shortcut=1
+wheel_scroll_lines=3
+EOF
+print_info "$version config configured successfully"
+  fi
+done
