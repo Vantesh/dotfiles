6545f9d7d444fb65c7e7b85d97c72137c62a38e2
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-08-30 13:52:28 +0300

    feat: Enhance UFW configuration in services script
    - Added steps to allow specific ports for LocalSend and SSH.

--- home/.chezmoiscripts/linux/run_once_before_02_services.sh
+++ home/.chezmoiscripts/linux/run_once_before_02_services.sh
@@ -40,8 +40,22 @@ for scope in user system; do
 done
 
 # Enable ufw
+print_step "Enabling UFW (Uncomplicated Firewall)"
 if sudo ufw enable >/dev/null 2>&1; then
   print_info "UFW enabled successfully"
+  # Allow ports for LocalSend
+  if sudo ufw allow 53317/udp >/dev/null 2>&1 && sudo ufw allow 53317/tcp >/dev/null 2>&1; then
+    print_info "Allowed ports for LocalSend"
+  else
+    print_warning "Failed to allow ports for LocalSend"
+  fi
+
+  if sudo ufw allow 22/tcp >/dev/null 2>&1; then
+    print_info "Allowed SSH port 22"
+  else
+    print_warning "Failed to allow SSH port 22"
+  fi
+
 else
   print_warning "Failed to enable UFW"
 fi
