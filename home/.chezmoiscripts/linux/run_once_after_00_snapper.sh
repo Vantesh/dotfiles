6a4e3cff6da3cb3c5ec17b7f65d602016cfed6bb
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-10-07 00:21:25 +0300

    feat: update DNF snapper plugin configuration
    - Refactor the way DNF snapper actions are written to the configuration file.

--- home/.chezmoiscripts/linux/run_once_after_00_snapper.sh
+++ home/.chezmoiscripts/linux/run_once_after_00_snapper.sh
@@ -316,19 +316,16 @@ configure_dnf_snapper_plugin() {
     return 0
   fi
 
-  local content
-  # SC2016: Variables in single quotes are intentional - they should not expand here
-  # shellcheck disable=SC2016
-  content='# /etc/dnf/libdnf5-plugins/actions.d/snapper.actions
+  if ! write_system_config "$actions_file" <<'EOF'; then
+# /etc/dnf/libdnf5-plugins/actions.d/snapper.actions
 # Get snapshot description
-pre_transaction::::/usr/bin/sh -c echo\ "tmp.cmd=$(ps\ -o\ command\ --no-headers\ -p\ '"'"'${pid}'"'"')"
+pre_transaction::::/usr/bin/sh -c echo\ "tmp.cmd=$(ps\ -o\ command\ --no-headers\ -p\ '${pid}')"
 # Creates pre snapshot before the transaction and stores the snapshot number in the "tmp.snapper_pre_number"  variable.
-pre_transaction::::/usr/bin/sh -c echo\ "tmp.snapper_pre_number=$(snapper\ create\ -c\ number\ -t\ pre\ -p\ -d\ '"'"'${tmp.cmd}'"'"')"
+pre_transaction::::/usr/bin/sh -c echo\ "tmp.snapper_pre_number=$(snapper\ create\ -c\ number\ -t\ pre\ -p\ -d\ '${tmp.cmd}')"
 
 # If the variable "tmp.snapper_pre_number" exists, it creates post snapshot after the transaction and removes the variable "tmp.snapper_pre_number".
-post_transaction::::/usr/bin/sh -c [\ -n\ "${tmp.snapper_pre_number}"\ ]\ &&\ snapper\ create\ -c\ number\ -t\ post\ --pre-number\ "${tmp.snapper_pre_number}"\ -d\ "${tmp.cmd}"\ ;\ echo\ tmp.snapper_pre_number\ ;\ echo\ tmp.cmd'
-
-  if ! write_system_config "$actions_file" "$content"; then
+post_transaction::::/usr/bin/sh -c [\ -n\ "${tmp.snapper_pre_number}"\ ]\ &&\ snapper\ create\ -c\ number\ -t\ post\ --pre-number\ "${tmp.snapper_pre_number}"\ -d\ "${tmp.cmd}"\ ;\ echo\ tmp.snapper_pre_number\ ;\ echo\ tmp.cmd
+EOF
     log ERROR "Failed to create DNF snapper actions: $LAST_ERROR"
     return 1
   fi
