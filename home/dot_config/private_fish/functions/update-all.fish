2bea95b8291b732ca1f2f506fd3fce31bf5c27ef
Vantesh <121645908+Vantesh@users.noreply.github.com>
2025-10-16 02:37:24 +0300

    chore(fish): remove update-all function

--- home/dot_config/private_fish/functions/update-all.fish
+++
@@ -1,7 +0,0 @@
-function update-all --wraps=topgrade --description 'alias update-all=topgrade'
-    if type -f topgrade &>/dev/null
-        topgrade -k --no-self-update $argv
-    else
-        missing_package topgrade
-    end
-end
